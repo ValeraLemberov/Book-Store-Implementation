3d1dc6ca7a2d93323b2ed9551b51286415d77624
Valera Lemberov <valera.lemberov1993@gmail.com>
2024-02-13 14:06:28 +0200

    Implemented pagination and tested all in Swagger(open API).

--- src/main/java/intro/bookservice/service/BookService.java
+++ src/main/java/intro/bookservice/service/BookService.java
@@ -4,11 +4,12 @@ import intro.bookservice.dto.BookDto;
 import intro.bookservice.dto.BookSearchParameters;
 import intro.bookservice.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto bookDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
